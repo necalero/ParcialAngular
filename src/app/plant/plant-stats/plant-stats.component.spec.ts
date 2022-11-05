298729b240f33fdc2678343cef914cadf8cec976
Nicolás Calero <ne.calero@uniandes.edu.co>
2022-11-05 17:12:14 -0500

    Punto 3

--- src/app/plant/plant-stats/plant-stats.component.spec.ts
+++ src/app/plant/plant-stats/plant-stats.component.spec.ts
@@ -4,6 +4,7 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { PlantStatsComponent } from './plant-stats.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PlantStatsComponent', () => {
   let component: PlantStatsComponent;
@@ -11,6 +12,7 @@ describe('PlantStatsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ PlantStatsComponent ]
     })
     .compileComponents();
