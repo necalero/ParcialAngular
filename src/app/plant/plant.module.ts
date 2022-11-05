4f5b79f97405742286ee400979c774d384270418
Nicolás Calero <ne.calero@uniandes.edu.co>
2022-11-05 15:51:01 -0500

    Punto 2

--- src/app/plant/plant.module.ts
+++ src/app/plant/plant.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlantListComponent } from './plant-list/plant-list.component';
+import { PlantStatsComponent } from './plant-stats/plant-stats.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports: [PlantListComponent],
-  declarations: [PlantListComponent]
+  exports: [PlantListComponent, PlantStatsComponent],
+  declarations: [PlantListComponent, PlantStatsComponent]
 })
 export class PlantModule { }
